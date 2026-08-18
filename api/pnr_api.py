f0c6224878a5f9f2acd1d38453d02b9ba34551fe
shivam yadav <pradhansurendra335@gmail.com>
2026-08-18 22:08:47 +0530

    fix confi import path for vercel

--- api/pnr_api.py
+++ api/pnr_api.py
@@ -1,3 +1,9 @@
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 import requests
 from config import API_KEY
 API_HOST = "irctc-indian-railway-pnr-status.p.rapidapi.com"
